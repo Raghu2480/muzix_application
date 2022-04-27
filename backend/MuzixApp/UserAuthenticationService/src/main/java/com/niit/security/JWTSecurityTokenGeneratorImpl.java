4f797f9f40702829f2883e67179271947ccc9439
kamran <mohammadkamran9910@gmail.com>
2022-04-27 13:39:33 +0530

    eureka server and spring cloud api gateway added

--- backend/MuzixApp/UserAuthenticationService/src/main/java/com/niit/security/JWTSecurityTokenGeneratorImpl.java
+++ backend/MuzixApp/UserAuthenticationService/src/main/java/com/niit/security/JWTSecurityTokenGeneratorImpl.java
@@ -1,25 +1,26 @@
-//package com.niit.security;
-//
-//import com.niit.domain.User;
-//import org.bouncycastle.jcajce.BCFKSLoadStoreParameter;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Date;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Service
-//public class JWTSecurityTokenGeneratorImpl implements SecurityTokenGenerator {
-////    public Map<String, String> generateToken(User user) {
-////// multiple claims for a token - 3 types - registered, public, and private
-//////        String jwtToken = Jwts.builder().setIssuer("ShopZone")
-////                .setSubject(user.getEmail())
-////                .setIssuedAt(new Date())
-////                .signWith(BCFKSLoadStoreParameter.SignatureAlgorithm.HS256,"mysecret")
-////                .compact();
-////        Map<String,String> map = new HashMap<>();
-////        map.put("token",jwtToken);
-////        map.put("message","Authentication Successful");
-////        return map;
-////    }
-//}
+package com.niit.security;
+
+import com.niit.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JWTSecurityTokenGeneratorImpl implements SecurityTokenGenerator {
+    public Map<String, String> generateToken(User user) {
+// multiple claims for a token - 3 types - registered, public, and private
+        String jwtToken = Jwts.builder().setIssuer("ShopZone")
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256,"mysecret")
+                .compact();
+        Map<String,String> map = new HashMap<>();
+        map.put("token",jwtToken);
+        map.put("message","Authentication Successful");
+        return map;
+    }
+}
